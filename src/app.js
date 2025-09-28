d0a4338141e8aa3b7cda51f2a82d304c07a473f4
Moisés <69405573+moisesEL@users.noreply.github.com>
2025-09-28 17:53:01 +0000

    commit final

--- src/app.js
+++ src/app.js
@@ -6,6 +6,25 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  
+let pronoun = ['el','nuestro','aquel','este'];
+let adj = ['genial','grande','pequeño','fantastico'];
+let noun = ['jueves','miercoles','viernes','lunes'];
+let extensions = ['.com','.org','.pro', '.es']
+
+  pronoun.forEach(pronoun => {
+    adj.forEach (adj => {
+      noun.forEach (noun => {
+        extensions.forEach (extension => {
+
+          if(extension === '.es') {
+            let domainhack = noun.slice(0, noun.length - 2);;
+            console.log( pronoun + adj + domainhack + extension);
+            } else console.log( pronoun + adj + noun + extension);
+      });
+      });
+    });
+  });
+
+
 };
